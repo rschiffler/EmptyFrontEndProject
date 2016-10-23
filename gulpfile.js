8e4befb3cc25c67f9602df60b3e76ea153ac1502
Rose Schiffler <rose.schiffler@gmail.com>
2016-10-23 20:45:15 +0100

    Add cache busting to css file

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var concat = require('gulp-concat');
 var hb = require('gulp-hb');
 var cleanCss = require ('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
+var rev = require('gulp-rev');
+var revReplace = require("gulp-rev-replace");
 
 var baseDir = 'src';
 var buildDir = 'build';
@@ -18,8 +20,8 @@ var dataDir = baseDir + '/data';
 // default task, run 'gulp'
 gulp.task('default', ['build', 'server'], function() { });
 
-/* BUILD FILES */
-gulp.task('build', ['clean', 'html', 'handlebars', 'styles', 'images']);
+/* BUILD ALL FILES */
+gulp.task('build', ['clean', 'styles', 'html', 'images', 'cache-bust']);
 
 gulp.task('clean', function() {
   gulp.src(buildDir + '/**/*.*', {read: false})
@@ -27,13 +29,7 @@ gulp.task('clean', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(baseDir + '/*.html')
-    .pipe(gulp.dest(buildDir));
-});
-
-gulp.task('handlebars', function() {
-  return gulp
-    .src(baseDir + '/html/*.html')
+  return gulp.src(baseDir + '/html/*.html')
     .pipe(hb({
       partials: partialsDir + '/**/*.hbs',
       data: dataDir + '/**/*.json'
@@ -42,18 +38,29 @@ gulp.task('handlebars', function() {
 });
 
 gulp.task('styles', function() {
-  gulp.src(stylesDir + '/main.styl')
+  return gulp.src(stylesDir + '/main.styl')
     .pipe(sourcemaps.init())
       .pipe(stylus())
       .pipe(concat('styles.css'))
+      .pipe(rev())
       .pipe(cleanCss({compatibility: 'ie8'}))
     .pipe(sourcemaps.write())
+    .pipe(gulp.dest(buildDir))
+    .pipe(rev.manifest())
     .pipe(gulp.dest(buildDir));
 });
 
 gulp.task('images', function() {
   gulp.src(imagesDir + '/*.*')
     .pipe(gulp.dest(buildImagesDir));
+});
+
+gulp.task('cache-bust', ['html', 'styles'], function() {
+  var manifest = gulp.src(buildDir + "/rev-manifest.json");
+
+  return gulp.src(buildDir + "/index.html")
+    .pipe(revReplace({manifest: manifest}))
+    .pipe(gulp.dest(buildDir));
 });
 /* END BUILD FILES */
 
